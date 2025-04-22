38b451ee12bb19344262d0c314e018c8f1c3ccc9
Demerson Máximo de Carvalho <dcarvalho@pdmfc.com>
2025-04-22 06:22:24 -0300

    Initial commit

---
+++ src/namespace/children/Enum.ts
@@ -0,0 +1,12 @@
+/**
+ * @enum {string}
+ * @readonly
+ * @summary Enumeration of options
+ * @description This enum provides a set of predefined options that can be used throughout the application.
+ * 
+ * @memberOf module:ts-workspace.Namespace.ChildNamespace
+ */
+export enum Enum {
+  /** Represents the first option with value "something" */
+  OPTION1 = "something",
+}
