a91e23a9879c62d95e44467ffc28f78aa55cbc2e
Demerson Carvalho <demersonmcarvalho@hotmail.com>
2025-09-10 08:46:49 -0300

    DECAF-122 - update build function

--- bin/build.js
+++ bin/build.js
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process';
-import { writeFileSync, cpSync, mkdirSync } from 'fs';
+import { readdirSync, statSync, cpSync, mkdirSync } from 'fs';
 import path from 'path';
 const packageJson = path.resolve('package.json');
 
@@ -11,20 +11,40 @@ execSync(`npx rimraf ${distDir} && npx rimraf ${libDir}`, { stdio: 'inherit' });
 mkdirSync(`${distDir}`, { recursive: true });
 mkdirSync(`${libDir}/dist`, { recursive: true });
 
-// Check for --prod flag
-const isProd = process.argv.includes('--prod');
-const buildCommand = isProd ? 
-'npx sass --no-source-map src/main.scss dist/main.css && npx postcss dist/main.css --use autoprefixer --use cssnano --no-map -o dist/main.min.css'
- : 'npx sass src/main.scss dist/main.css';
+const production = process.argv.includes('--prod');
+
+function getAllScssFiles(dir) {
+  const files = readdirSync(dir);
+  let scssFiles = [];
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    if (statSync(filePath).isDirectory()) {
+      scssFiles = scssFiles.concat(getAllScssFiles(filePath));
+    } else if (file.endsWith('.scss')) {
+      scssFiles.push(filePath);
+    }
+  });
+  return scssFiles;
+}
+
+const files = getAllScssFiles(sourceDir);
+
+function buildFile(file, outFile) {
+    const buildCommand = production ? 
+        `npx sass --no-source-map ${file} ${outFile} && npx postcss ${outFile} --use autoprefixer --use cssnano --no-map -o ${outFile.replace('.css', '.min.css')}`
+        : `npx sass ${file} ${outFile}`;
+    console.log(`Building: ${file} -> ${outFile}`);
+    execSync(buildCommand, { stdio: 'inherit' });
+}
+
+files.forEach(file => {
+  const outFile = path.join(distDir, path.relative(sourceDir, file).replace('.scss', '.css'));
+  mkdirSync(path.dirname(outFile), { recursive: true });
+  buildFile(file, outFile);
+});
 
 
-// Build development CSS
-execSync(buildCommand, { stdio: 'inherit' });
-
-
-// Copia package.json para lib/
 cpSync(packageJson, path.join(libDir, 'package.json'));
 
-// Copia tudo da pasta dist/ para lib/
 cpSync(distDir, `${libDir}/dist`, { recursive: true });
 cpSync(sourceDir, `${libDir}/src`, { recursive: true });
